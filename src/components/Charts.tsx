fd88df70cb352b7365ea15c702f513cdbab02e35
Viveksingh99 <vivek22jan99@gmail.com>
2024-08-20 00:39:22 +0530

    add

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,73 +1,93 @@
-"use client";
+
+"use client"
+import React, { useState } from "react";
 import {
   BarChart,
   Bar,
-  YAxis,
+  Cell,
   XAxis,
+  YAxis,
+  CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+const Example = () => {
+  const [data] = useState([
+    {
+      name: "Page A",
+      uv: 100,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 90,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 80,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 30,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 10,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 90,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 40,
+      pv: 4300,
+      amt: 2100,
+    },
+  ]);
 
-export default function Charts() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (data: any, index: any) => {
+    setActiveIndex(index);
+  };
+
+  const activeItem = data[activeIndex];
+
   return (
-    <main className="mx-auto m-24">
-      <div className="h-72 w-full">
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart width={48} height={48} data={data}>
+    <div style={{ width: "100%" }}>
+      <p>Click each rectangle</p>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart width={150} height={40} data={data}>
+          <Bar dataKey="uv" onClick={handleClick}>
+            {data.map((entry, index) => (
+              <Cell
+                cursor="pointer"
+                fill={index === activeIndex ? "#82ca9d" : "#8884d8"}
+                key={`cell-${index}`}
+              />
+            ))}
+          </Bar>
+          {/* <Tooltip formatter={(value) => `${value}%`} /> */}
           <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="uv" fill="#8884d8" />
+          <YAxis tickFormatter={(tick) => `${tick}%`} />
         </BarChart>
       </ResponsiveContainer>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Example;
